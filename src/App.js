977aa35b78258668f5b38fe9629c139d58e3a0ba
ts-priyansh.gaharana <ts-priyansh.gaharana@rakuten.com>
2021-01-24 16:08:53 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function isDate(val) {
+        // Cross realm comptatible
+        return Object.prototype.toString.call(val) === "[object Date]";
+    }
+
+    function isObj(val) {
+        return typeof val === "object";
+    }
+
+    function stringifyValue(val) {
+        if (isObj(val) && !isDate(val)) {
+            return JSON.stringify(val);
+        } else {
+            return val;
+        }
+    }
+
+    function buildForm({ action, params }) {
+        const form = document.createElement("form");
+        form.setAttribute("method", "post");
+        form.setAttribute("action", action);
+
+        Object.keys(params).forEach((key) => {
+            const input = document.createElement("input");
+            input.setAttribute("type", "hidden");
+            input.setAttribute("name", key);
+            input.setAttribute("value", stringifyValue(params[key]));
+            form.appendChild(input);
+        });
+
+        return form;
+    }
+
+    function post(details) {
+        const form = buildForm(details);
+        document.body.appendChild(form);
+        form.submit();
+        form.remove();
+    }
+
+    const handlePayment = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:5000/api/payment", {
+                amount: 5000,
+                email: "abc@gmail.comup",
+                phoneno: "9876543211",
+            })
+            .then((res) => {
+                console.log(res.data);
+                let info = {
+                    action: "https://securegw-stage.paytm.in/order/process",
+                    params: res.data,
+                };
+                post(info);
+            });
+    };
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={handlePayment}> Pay </button>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
